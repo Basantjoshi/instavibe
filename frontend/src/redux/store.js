9d9bfad440ef03be01d3ccaf00c3a2ce9331e973
Basantjoshi <99581815+Basantjoshi@users.noreply.github.com>
2025-03-23 20:44:41 +0545

    ready for deployment

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,55 @@
+import { combineReducers, configureStore} from "@reduxjs/toolkit";
+import authSlice from "./authSlice.js";
+import postSlice from './postSlice.js';
+import socketSlice from "./socketSlice.js"
+import chatSlice from "./chatSlice.js";
+import rtnSlice from "./rtnSlice.js";
+
+import { 
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+}
+
+const rootReducer = combineReducers({
+    auth:authSlice,
+    post:postSlice,
+    socketio:socketSlice,
+    chat:chatSlice,
+    realTimeNotification:rtnSlice
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: {
+            // Ignore these action types
+            ignoredActions: ['your/action/type'],
+            // Ignore these field paths in all actions
+            ignoredActionPaths: ['meta.arg', 'payload.timestamp'],
+            // Ignore these paths in the state
+            ignoredPaths: ['items.dates'],
+          },
+          serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+        
+        }),
+});
+
+export default store;
